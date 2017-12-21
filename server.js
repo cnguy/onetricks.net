9307e9b82212327f255485f0670a096077ac0ddf
Chau Nguyen <chautnguyen96@gmail.com>
2017-12-20 21:06:28 -0800

    docker test stuffs

--- server.js
+++ server.js
@@ -7,111 +7,12 @@ app.use(compression());
 
 require('dotenv').config();
 
-const mongoose = require('mongoose');
-require('./models');
-
-const Player = mongoose.model('Player');
-
-if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.LOCAL_MONGO_URL);
-} else {
-  mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds161029.mlab.com:61029/${process.env.MONGO_USER}`);
-}
-
 // Express only serves static assets in production.
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
 app.set('port', (process.env.PORT || 3001));
-
-app.get('/all', (req, res, next) => {
-  const multiple = req.query.multiple || false;
-
-  if (multiple) {
-    const _regions = req.query.region.split(',') || null;
-
-    if (regions) {
-      Player.find({ region: { $in: _regions } }, (err, players) => {
-        if (err) return next(err);
-        res.json(players);
-      });
-    }
-  } else {
-    const region = req.query.region || null;
-
-    if (region && region !== 'all') {
-      Player.find({ region }, (err, players) => {
-        if (err) return next(err);
-        res.json(players);
-      });
-    } else {
-      Player.find((err, players) => {
-        if (err) return next(err);
-        res.json(players);
-      });
-    }
-  }
-});
-
-app.get('/masters', (req, res) => {
-  const multiple = req.query.multiple || false;
-
-  if (multiple) {
-    const _regions = req.query.region.split(',') || null;
-
-    if (regions) {
-      Player.find({ region: { $in: _regions } }, (err, players) => {
-        if (err) return next(err);
-        res.json(players);
-      });
-    }
-  }
-  else {
-    const region = req.query.region || null;
-
-    if (region && region !== 'all') {
-      Player.find({ rank: 'm', region }, (err, masters, next) => {
-        if (err) return next(err);
-        res.json(masters);
-      });
-    } else {
-      Player.find({ rank: 'm' }, (err, masters, next) => {
-        if (err) return next(err);
-        res.json(masters);
-      });
-    }
-  }
-});
-
-app.get('/challengers', (req, res) => {
-  const multiple = req.query.multiple || false;
-
-  if (multiple) {
-    const _regions = req.query.region.split(',') || null;
-
-    if (regions) {
-      Player.find({ region: { $in: _regions } }, (err, players) => {
-        if (err) return next(err);
-        res.json(players);
-      });
-    }
-  } else {
-    const region = req.query.region || null;
-
-    if (region && region !== 'all') {
-      Player.find({ rank: 'c', region }, (err, challengers, next) => {
-        if (err) return next(err);
-        res.json(challengers);
-      });
-    } else {
-      Player.find({ rank: 'c' }, (err, challengers, next) => {
-        if (err) return next(err);
-        res.json(challengers);
-      });
-    }
-  }
-});
 
 app.use((req, res, next) =>
   res.render('404', { status: 404, url: req.url }),
