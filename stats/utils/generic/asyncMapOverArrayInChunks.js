7fcba71c44fbca676870cce909b4e1eebf60b028
Chau Nguyen <chautnguyen96@gmail.com>
2018-01-03 04:15:00 -0800

    refactor (Stats): Fix old naming in asyncMapOverArrayInChunks to be generic

--- stats/utils/generic/asyncMapOverArrayInChunks.js
+++ stats/utils/generic/asyncMapOverArrayInChunks.js
@@ -2,10 +2,10 @@ const asyncMapOverArrayInChunks = async (array, chunkSize, mapFunction) => {
     // Mutation
     let results = [];
     for (let i = 0; i < array.length; i += chunkSize) {
-        const playerStatsArray = await Promise.all(
+        const currentResults = await Promise.all(
             array.slice(i, i + chunkSize).map(mapFunction),
         );
-        results = results.concat(playerStatsArray);
+        results = results.concat(currentResults);
     }
     return results;
 };
