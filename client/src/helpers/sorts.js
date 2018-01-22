724d581c9d473834fd2aec3d31bca9de5d58cfd4
Chau <ChauTNguyen96@gmail.com>
2018-01-22 13:03:13 -0800

    refactor (client): Clean up dead code

--- client/src/helpers/sorts.js
+++ client/src/helpers/sorts.js
@@ -1,19 +1,6 @@
 import { sortBy } from 'lodash'
 
 const SORTS = {
-    NONE: list => list,
-    RANK: list => sortBy(list, ['rank', 'name']),
-    REGION: list => sortBy(list, ['region', 'rank']),
-    NAME: list =>
-        sortBy(list, [l => l.name.toLowerCase().replace(/\s+/g, '')], ['name']),
-    WINS: list => sortBy(list, ['wins', 'rank', 'name']),
-    LOSSES: list => sortBy(list, ['losses', 'rank', 'name']),
-    // Structure-specific sort method
-    WINRATE: list =>
-        list.sort(
-            (a, b) =>
-                a.wins / (a.wins + a.losses) - b.wins / (b.wins + b.losses),
-        ),
     ONETRICKS: list =>
         list.sort((a, b) => {
             if (a[1].length > b[1].length) return -1
