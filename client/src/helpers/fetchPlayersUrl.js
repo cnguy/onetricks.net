0d0a8a55e0eb4e001c1fc91885041ef5013afb07
Chau <ChauTNguyen96@gmail.com>
2018-03-15 08:44:41 -0700

    fix (Client): Remove unnecessary cors request for production

--- client/src/helpers/fetchPlayersUrl.js
+++ client/src/helpers/fetchPlayersUrl.js
@@ -1,7 +1,7 @@
-const fetchPlayersUrl = `https://cors-anywhere.herokuapp.com/http://${
+const fetchPlayersUrl = `${
     process.env.NODE_ENV === 'production'
-        ? '104.131.26.226:3001'
-        : '8b733b23.ngrok.io'
+        ? 'http://104.131.26.226:3001'
+        : 'https://cors-anywhere.herokuapp.com/http://8b733b23.ngrok.io'
 }/all?region=all`
 
 export default fetchPlayersUrl
