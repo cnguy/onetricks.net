01684fa96ad338e0b1f5c7987ce46b6e229c4669
Chau <ChauTNguyen96@gmail.com>
2018-03-19 20:01:33 -0700

    fix (Client): Remove corsanywhere now that we enabled it on api side. Add makefile scripts for building images too

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
 run:
 	docker-compose up --build;
 
+build-api:
+	cd api;\
+	sudo docker build . -t cnguy/one-tricks-api
+
+build-stats:
+	cd stats;\
+	sudo docker build . -t cnguy/one-tricks-stats
+
 run-prod:
 	docker-compose -f docker-compose.prod.yml pull;\
 	docker-compose -f docker-compose.prod.yml up --build;
--- client/src/helpers/fetchPlayersUrl.js
+++ client/src/helpers/fetchPlayersUrl.js
@@ -1,7 +1,7 @@
-const fetchPlayersUrl = `https://cors-anywhere.herokuapp.com/${
+const fetchPlayersUrl = `${
     process.env.NODE_ENV === 'production'
         ? 'http://104.131.26.226:3001'
-        : 'http://8b733b23.ngrok.io'
+        : 'https://cors-anywhere.herokuapp.com/http://8b733b23.ngrok.io'
 }/all?region=all`
 
 export default fetchPlayersUrl
